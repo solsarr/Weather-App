97325650e9fb21ab551d3dc719689109a2ecaebb
alex-willi <alex.lawson.williams@gmail.com>
2022-12-09 10:36:43 -0600

    css solomon

--- src/components/Data.js
+++ src/components/Data.js
@@ -42,46 +42,52 @@ export default function Data() {
 
   return (
     <div>
-      <button
-        className="3day"
-        onClick={() => {
-          handleClick();
-          setSevenDay(false);
-          setCurrentDay(false);
-        }}
-        disabled={threeDay ? true : false}
-      >
-        3 Day
-      </button>
+      <div className="dataWrap" style={{ border: "1px solid black" }}>
+        <div className="time" style={{ border: "1px solid black" }}>
+          <DateTime />
+        </div>
+        {threeDay && <Forecast3 {...data} />}
 
-      <button
-        className="7day"
-        onClick={() => {
-          handleDay();
-          setThreeDay(false);
-          setCurrentDay(false);
-        }}
-        disabled={sevenDay ? true : false}
-      >
-        1 Week
-      </button>
-      <button
-        className="today"
-        onClick={() => {
-          handleToday();
-          setSevenDay(false);
-          setThreeDay(false);
-        }}
-        disabled={currentDay ? true : false}
-      >
-        Today
-      </button>
-      {threeDay && <Forecast3 {...data} />}
+        {currentDay && <Today {...data} />}
 
-      {currentDay && <Today {...data} />}
+        {sevenDay && <Forecast7 {...data} />}
+      </div>
+      <div className="btnWrap" style={{ border: "1px solid black" }}>
+        <button
+          className="three"
+          onClick={() => {
+            handleClick();
+            setSevenDay(false);
+            setCurrentDay(false);
+          }}
+          disabled={threeDay ? true : false}
+        >
+          3 Day
+        </button>
 
-      {sevenDay && <Forecast7 {...data} />}
-      <DateTime />
+        <button
+          className="seven"
+          onClick={() => {
+            handleDay();
+            setThreeDay(false);
+            setCurrentDay(false);
+          }}
+          disabled={sevenDay ? true : false}
+        >
+          1 Week
+        </button>
+        <button
+          className="todayBtn"
+          onClick={() => {
+            handleToday();
+            setSevenDay(false);
+            setThreeDay(false);
+          }}
+          disabled={currentDay ? true : false}
+        >
+          Today
+        </button>
+      </div>
     </div>
   );
 }
--- src/components/Three.js
+++ src/components/Three.js
@@ -1,17 +1,23 @@
 export default function Forecast3(data) {
   return (
-    <div className="7day">
-      <div className="day1">
+    <div className="threeData">
+      <div
+        className="dayOne"
+        style={{ border: "1px solid black", borderColor: "rgba(0, 0, 0, .2)" }}
+      >
         <p>
           {" "}
-          Date Today {data.daily.time[0].slice(5)}
-          Low: {data.daily.temperature_2m_min[0]}F - High:{" "}
+          Date Today {data.daily.time[0].slice(5)} Low:{" "}
+          {data.daily.temperature_2m_min[0]}F - High:{" "}
           {data.daily.temperature_2m_max[0]}F - Inches of rain:{" "}
           {data.daily.precipitation_sum[0]}"
         </p>
       </div>
 
-      <div className="day2">
+      <div
+        className="dayTwo"
+        style={{ border: "1px solid black", borderColor: "rgba(0, 0, 0, .2)" }}
+      >
         <p>
           {" "}
           Date {data.daily.time[1].slice(5)} Low:{" "}
@@ -21,7 +27,10 @@ export default function Forecast3(data) {
         </p>
       </div>
 
-      <div className="day3">
+      <div
+        className="dayThree"
+        style={{ border: "1px solid black", borderColor: "rgba(0, 0, 0, .2)" }}
+      >
         <p>
           {" "}
           Date {data.daily.time[2].slice(5)} Low:{" "}
